e1078a78f612dee54d942ce12fbae6b6ca819ac9
ryanpilar <ryan_pilar@outlook.com>
2022-04-07 08:07:33 -0600

    code wars solution

---
+++ js/arrayDiff-6kyu.js
@@ -0,0 +1,12 @@
+function arrayDiff(a, b) {
+    // https://www.codewars.com/kata/523f5d21c841566fde000009/train/javascript
+
+    let newA = [...a]
+    b.forEach( num => { newA = newA.filter( e => e !== num ) })
+    return newA
+}
+
+const a = [1,2,2,2,2,3,4,5,5]
+const b = [2, 1]
+
+
