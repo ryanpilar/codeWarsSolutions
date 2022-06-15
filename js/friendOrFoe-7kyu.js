fd0933ceca3233b8d31f9ab55b03f567d84c073f
ryanpilar <ryan_pilar@outlook.com>
2022-06-15 08:47:15 -0600

    codewars solution

---
+++ js/friendOrFoe-7kyu.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/55b42574ff091733d900002f/train/javascript
+
+function friend(friends){
+    return fwends = friends.filter( name => name.length === 4 )
+}
+
+// const friends = ["Ryan", "Kieran", "Mark"] //  ["Ryan", "Mark"])
+// const friends = ["Ryan", "Jimmy", "123", "4", "Cool Man"] //  ["Ryan"])
+// const friends = ["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"] //  ["Jimm", "Cari", "aret"])
+const friends = ["Love", "Your", "Face", "1"] // ["Love", "Your", "Face"]
+
+console.log(friend(friends))
