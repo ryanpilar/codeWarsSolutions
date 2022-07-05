ffbcb2cdb6c8d14e974578a5839d2de1f881c322
ryanpilar <ryan_pilar@outlook.com>
2022-07-04 18:01:24 -0600

    codewars solution

---
+++ js/printingArrayElement-8kyu.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/javascript
+function printArray(array){
+    return array.join(',')
+}
+
+const array = [2,4,5,2]
+console.log(printArray(array));
