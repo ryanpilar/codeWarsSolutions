0c63a768025ecd260762dec870a9a6d7fb05babb
ryanpilar <ryan_pilar@outlook.com>
2022-07-08 18:00:11 -0600

    codewars solution

---
+++ js/myHeadIsAtTheWrongEnd-8kyu.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/56f699cd9400f5b7d8000b55
+
+function fixTheMeerkat(arr) {
+    return [ arr[2], arr[1], arr[0] ] 
+}
+
+
+const arr = ["bottom", "middle", "top"]
+console.log(fixTheMeerkat(arr));
