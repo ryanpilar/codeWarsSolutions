f2b0a349a75085c97d9ff604c0a9f6a573e02242
ryanpilar <ryan_pilar@outlook.com>
2022-07-25 02:44:05 -0600

    codewars solution

---
+++ js/convertStringToArray-8kyu.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/javascript
+
+function stringToArray(string){
+
+	// code code code
+    return string.split(' ')
+}
+
+const string = "I love arrays they are my favorite"
+console.log(stringToArray(string));
