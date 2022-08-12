ed447f5b4dfa9f37af82088fc326d9242540379a
ryanpilar <ryan_pilar@outlook.com>
2022-08-12 02:35:32 -0600

    codewars solution

---
+++ js/removeStringSpaces-8kyu.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
+
+function noSpace(x){
+    return x.split(' ').join('')
+}
+
+const x = 'hello you kk pp'
+console.log(noSpace(x));
