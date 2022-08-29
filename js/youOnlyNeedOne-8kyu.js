a6416cba6c795453a800a20df94ebb8e54ca52a6
ryanpilar <ryan_pilar@outlook.com>
2022-08-29 02:11:45 -0600

    codewars solution

---
+++ js/youOnlyNeedOne-8kyu.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/57cc975ed542d3148f00015b
+
+function check(a, x) {
+    // your code here
+    return a.includes(x)
+}
+
+const a = [101, 45, 75, 105, 99, 107]
+const x = 108
+
+console.log(check(a, x));
