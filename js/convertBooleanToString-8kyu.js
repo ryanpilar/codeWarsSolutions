5c6d7c1a4c4c6846573cc47e28b6fa13eccc7471
ryanpilar <ryan_pilar@outlook.com>
2022-09-02 00:35:57 -0600

    codewars solution

---
+++ js/convertBooleanToString-8kyu.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
+
+function booleanToString(b){
+    return b.toString()
+}
+
+
+const b = false
+console.log(booleanToString(b));
