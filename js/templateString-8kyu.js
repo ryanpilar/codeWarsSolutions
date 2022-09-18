f8812099ab8826a2b4bbaefeda61b3b8d65d8181
ryanpilar <ryan_pilar@outlook.com>
2022-09-18 14:10:01 -0600

    codewars solution

---
+++ js/templateString-8kyu.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55a14f75ceda999ced000048
+
+var TempleStrings = function(obj, feature) {
+    return `${obj} are ${feature}`
+}
+
+console.log(TempleStrings("Animals","Good"));
