f313891ff9a0d3ea9f6b5ebcf008fbcabe10cd2b
ryanpilar <ryan_pilar@outlook.com>
2022-09-30 13:53:23 -0600

    codewars solution

---
+++ js/welcomeToTheCity-8kyu.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5302d846be2a9189af0001e4/train/javascript
+
+function sayHello( name, city, state ) {
+
+    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+}
+
+console.log(sayHello( ['Wallace','Russel','Osbourne'],'Albany','New York' ));
